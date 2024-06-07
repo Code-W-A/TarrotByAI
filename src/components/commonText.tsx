7ba19cfad762a40b8d0f793507baa2d16f880a61
Adrian <radu.p1995@yahoo.com>
2024-06-07 16:49:03 +0300

    GOOD VERSION BEFORE START AUTH

--- src/components/commonText.tsx
+++ src/components/commonText.tsx
@@ -3,8 +3,6 @@ import React, { Component } from "react";
 import { View, Text, StyleSheet, TextProps } from "react-native";
 import { colors } from "../utils/colors";
 
-import styled from "styled-components";
-
 // define your styles
 const styles = StyleSheet.create({
   black: {
@@ -174,372 +172,372 @@ const {
 } = styles;
 const { fontBold, fontRegular, fontMedium } = styles;
 
-export const H2fontBoldPrimary = styled(Text)`
+export const H2fontBoldPrimary = `
   ${h2};
   ${fontBold};
   ${primary2};
 `;
-export const H3fontBoldPrimary = styled(Text)`
+export const H3fontBoldPrimary = `
   ${h3};
   ${fontBold};
   ${primary2};
 `;
-export const H6fontBoldPrimary = styled(Text)`
+export const H6fontBoldPrimary = `
   ${h3};
   ${fontBold};
   ${primary2};
 `;
-export const H6fontMediumPrimary = styled(Text)`
+export const H6fontMediumPrimary = `
 ${h6};
 ${fontMedium};
 ${primary2};
 `;
-export const H6fontRegularBlue = styled(Text)`
+export const H6fontRegularBlue = `
   ${h6};
   ${fontRegular};
   ${lightBlue};
 `;
-export const H8fontRegularRed = styled(Text)`
+export const H8fontRegularRed = `
   ${h8};
   ${fontRegular};
   ${darkRed};
 `;
-export const H6fontRegularBlack = styled(Text)`
+export const H6fontRegularBlack = `
   ${h6};
   ${fontRegular};
   ${black};
 `;
-export const H10fontRegularBlack = styled(Text)`
+export const H10fontRegularBlack = `
   ${h10};
   ${fontRegular};
   ${black};
 `;
-export const H10fontRegularLight = styled(Text)`
+export const H10fontRegularLight = `
   ${h10};
   ${fontRegular};
   ${textLight};
 `;
-export const H10fontRegularWhite = styled(Text)`
+export const H10fontRegularWhite = `
   ${h10};
   ${fontRegular};
   ${primary2};
 `;
-export const H10fontRegularPrimary = styled(Text)`
+export const H10fontRegularPrimary = `
   ${h10};
   ${fontRegular};
   ${primary2};
 `;
-export const H10fontRegularGreen = styled(Text)`
+export const H10fontRegularGreen = `
   ${h10};
   ${fontRegular};
   ${green};
 `;
-export const H10fontRegularRed = styled(Text)`
+export const H10fontRegularRed = `
   ${h10};
   ${fontRegular};
   ${darkRed};
 `;
-export const H8fontRegularBlack = styled(Text)`
+export const H8fontRegularBlack = `
   ${h8};
   ${fontRegular};
   ${black};
 `;
 
-export const H8fontRegularSecondaryBlack = styled(Text)`
+export const H8fontRegularSecondaryBlack = `
   ${h8};
   ${fontRegular};
   ${secondaryBlack};
 `;
 
-export const H8fontRegularWhite = styled(Text)`
+export const H8fontRegularWhite = `
   ${h8};
   ${fontRegular};
   ${white};
 `;
 
-export const H6fontRegularPrimary = styled(Text)`
+export const H6fontRegularPrimary = `
   ${h6};
   ${fontRegular};
   ${primary2};
 `;
-export const H6fontBoldWhite = styled(Text)`
+export const H6fontBoldWhite = `
   ${h6};
   ${fontRegular};
   ${white};
 `;
-export const H6fontRegularWhite = styled(Text)`
+export const H6fontRegularWhite = `
   ${h6};
   ${fontRegular};
   ${white};
 `;
 
-export const H6fontMediumWhite = styled(Text)`
+export const H6fontMediumWhite = `
   ${h6};
   ${fontMedium};
   ${white};
 `;
 
-export const H22fontMediumBlack = styled(Text)`
+export const H22fontMediumBlack = `
   ${h22};
   ${fontMedium};
   ${black};
 `;
 
-export const H9fontRegularBlack = styled(Text)`
+export const H9fontRegularBlack = `
   ${h9};
   ${fontRegular};
   ${black};
 `;
-export const H9fontRegularGreen = styled(Text)`
+export const H9fontRegularGreen = `
   ${h9};
   ${fontRegular};
   ${green};
 `;
 
-export const H13fontRegularGray = styled(Text)`
+export const H13fontRegularGray = `
   ${h13};
   ${fontRegular};
   ${grayText};
 `;
 
-export const H14fontRegularBlack = styled(Text)`
+export const H14fontRegularBlack = `
   ${h14};
   ${fontRegular};
   ${black};
 `;
-export const H14fontRegularWhite = styled(Text)`
+export const H14fontRegularWhite = `
   ${h14};
   ${fontRegular};
   ${white};
 `;
-export const H14fontRegularLightBlack = styled(Text)`
+export const H14fontRegularLightBlack = `
   ${h14};
   ${fontRegular};
   ${lightBlack};
 `;
-export const H14fontRegularLightGray = styled(Text)`
+export const H14fontRegularLightGray = `
   ${h14};
   ${fontRegular};
   ${lightGray};
 `;
-export const H14fontRegularGray = styled(Text)`
+export const H14fontRegularGray = `
   ${h14};
   ${fontRegular};
   ${grayText};
 `;
-export const H14fontRegularBlackk = styled(Text)`
+export const H14fontRegularBlackk = `
   ${h14};
   ${fontRegular};
   ${black};
 `;
-export const H14fontRegularImage = styled(Text)`
+export const H14fontRegularImage = `
   ${h14};
   ${fontRegular};
   ${imageGray};
 `;
-export const H14fontMediumBlack = styled(Text)`
+export const H14fontMediumBlack = `
   ${h14};
   ${fontMedium};
   ${black};
 `;
-export const H14fontRegulargray = styled(Text)`
+export const H14fontRegulargray = `
   ${h14};
   ${fontRegular};
   ${lightGray};
 `;
 
-export const H14fontRegularRed = styled(Text)`
+export const H14fontRegularRed = `
   ${h14};
   ${fontRegular};
   ${red};
 `;
-export const H14fontRegularBlue = styled(Text)`
+export const H14fontRegularBlue = `
   ${h14};
   ${fontRegular};
   ${lightBlue};
 `;
 
-export const H18fontRegularBlackk = styled(Text)`
+export const H18fontRegularBlackk = `
   ${h18};
   ${fontRegular};
   ${black};
 `;
 
-export const H9fontRegularGray = styled(Text)`
+export const H9fontRegularGray = `
   ${h9};
   ${fontRegular};
   ${lightGray};
 `;
 
-export const H8fontRegularPrimary = styled(Text)`
+export const H8fontRegularPrimary = `
   ${h8};
   ${fontRegular};
   ${primary2};
 `;
 
-export const H23fontRegularGray = styled(Text)`
+export const H23fontRegularGray = `
   ${h23};
   ${fontRegular};
   ${white};
 `;
 
-export const H16fontRegularGray = styled(Text)`
+export const H16fontRegularGray = `
   ${h16};
   ${fontRegular};
   ${white};
 `;
 
-export const H16fontRegularYellow = styled(Text)`
+export const H16fontRegularYellow = `
   ${h16};
   ${fontRegular};
   ${yellow};
 `;
 
-export const H7fontMediumBlack = styled(Text)`
+export const H7fontMediumBlack = `
   ${h7};
   ${fontMedium};
   ${black};
 `;
-export const H7fontMediumPrimary = styled(Text)`
+export const H7fontMediumPrimary = `
   ${h7};
   ${fontMedium};
   ${primary2};
 `;
-export const H7fontBoldPrimary = styled(Text)`
+export const H7fontBoldPrimary = `
   ${h7};
   ${fontBold};
   ${primary2};
 `;
-export const H7fontBoldWhite = styled(Text)`
+export const H7fontBoldWhite = `
   ${h7};
   ${fontBold};
   ${white};
 `;
-export const H7fontLightBlue = styled(Text)`
+export const H7fontLightBlue = `
   ${h7};
   ${fontMedium};
   ${lightBlue};
 `;
-export const H7fontRegularBlack = styled(Text)`
+export const H7fontRegularBlack = `
   ${h7};
   ${fontRegular};
   ${black};
 `;
-export const H7fontRegularWhite = styled(Text)`
+export const H7fontRegularWhite = `
   ${h7};
   ${fontRegular};
   ${white};
 `;
-export const H7fontMediumWhite = styled(Text)`
+export const H7fontMediumWhite = `
   ${h7};
   ${fontMedium};
   ${white};
 `;
 
-export const H9fontMediumBlack = styled(Text)`
+export const H9fontMediumBlack = `
   ${h9};
   ${fontMedium};
   ${black};
 `;
 
-export const H9fontMediumWhite = styled(Text)`
+export const H9fontMediumWhite = `
   ${h9};
   ${fontMedium};
   ${white};
 `;
 
-export const H9fontMediumBlue = styled(Text)`
+export const H9fontMediumBlue = `
   ${h9};
   ${fontMedium};
   ${lightBlue};
 `;
 
-export const H8fontMediumBlue = styled(Text)`
+export const H8fontMediumBlue = `
   ${h8};
   ${fontMedium};
   ${lightBlue};
 `;
 
-export const H15fontMediumBlack = styled(Text)`
+export const H15fontMediumBlack = `
   ${h15};
   ${fontMedium};
   ${black};
 `;
 
-export const H15fontMediumWhite = styled(Text)`
+export const H15fontMediumWhite = `
   ${h15};
   ${fontMedium};
   ${white};
 `;
 
-export const H8fontMediumBlack = styled(Text)`
+export const H8fontMediumBlack = `
   ${h8};
   ${fontMedium};
   ${black};
 `;
 
-export const H18fontMediumBlack = styled(Text)`
+export const H18fontMediumBlack = `
   ${h18};
   ${fontMedium};
   ${black};
 `;
 
-export const H8fontMediumWhite = styled(Text)`
+export const H8fontMediumWhite = `
   ${h8};
   ${fontMedium};
   ${white};
 `;
-export const H8fontBoldPrimary = styled(Text)`
+export const H8fontBoldPrimary = `
   ${h8};
   ${fontBold};
   ${primary2};
 `;
-export const H8fontMediumPrimary = styled(Text)`
+export const H8fontMediumPrimary = `
   ${h8};
   ${fontMedium};
   ${primary2};
 `;
 
-export const H8fontMediumLightBlack = styled(Text)`
+export const H8fontMediumLightBlack = `
   ${h8};
   ${fontMedium};
   ${textLight};
 `;
 
-export const H9fontMediumLightBlack = styled(Text)`
+export const H9fontMediumLightBlack = `
   ${h9};
   ${fontMedium};
   ${textLight};
 `;
-export const H30fontRegularLightBlack = styled(Text)`
+export const H30fontRegularLightBlack = `
   ${h8};
   ${fontRegular};
   ${textLight};
 `;
-export const H30fontRegularLightBlack2 = styled(Text)`
+export const H30fontRegularLightBlack2 = `
   ${h30};
   ${fontRegular};
   ${textLight};
 `;
-export const H30fontRegularLightRed = styled(Text)`
+export const H30fontRegularLightRed = `
   ${h8};
   ${fontRegular};
   ${red};
 `;
 
-export const H7fontRegularLight = styled(Text)`
+export const H7fontRegularLight = `
   ${h7};
   ${fontRegular};
   ${textLight};
 `;
-export const FormErrorMessage = styled(Text)`
+export const FormErrorMessage = `
   margin-top: 2px;
   ${h9};
   ${fontRegular};
   ${red}
 `;
 
-export const TCMessage = styled(Text)`
+export const TCMessage = `
   ${h14};
   ${fontMedium};
   ${lightBlue}
--- src/components/commonViews.tsx
+++ src/components/commonViews.tsx
@@ -7,7 +7,7 @@ import {
   ImageBackground,
   Platform,
 } from "react-native";
-import styled from "styled-components";
+
 import { colors } from "../utils/colors";
 
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
